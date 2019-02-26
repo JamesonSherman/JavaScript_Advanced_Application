a99bced9dfd0ade8bd795fb3168c6f54c958c694
James.Sherman <31998788+JamesonSherman@users.noreply.github.com>
2019-02-26 14:51:31 -0600

    Uploading of tcp crypto chat system.
    minimal online TCP based chat system using crypto encrypt/decrypt library sets

---
+++ tcp_cryptochat/server.js
@@ -0,0 +1,47 @@
+const crypto = require('crypto');
+let net = require('net');
+const port = 1234;
+const host = '127.0.0.1';
+let sockets = [];
+process.stdin.resume()
+process.stdin.setEncoding('utf8');
+/*
+net.createServer(function(s){
+
+    console.log('connected: ' + s.remoteAddress + ':' + s.remotePort);
+    s.on('data', function (data){
+        broadcast(">" + data + '\n');
+        s.write('you said: ' + data + ".");
+
+        s.broadcast(data);
+    })
+    function broadcast(message){
+        s.client
+    }
+}).listen(1234);
+console.log("chat server running at port 1234\n");
+
+*/
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port +'.');
+});
+
+server.on('connection', function (sock){
+console.log('connected: ' + sock.remoteAddress + ':' + sock.remotePort);
+sockets.push(sock);
+
+
+sock.on('data', function(data){
+sockets.forEach(function(sock,index,array){
+
+    let decipher = crypto.createDecipher('aes-256-cbc', 'admin123');
+    let decrypted = Buffer.concat([decipher.update(data), decipher.final()]);
+    let val = JSON.parse(decrypted.toString());
+    console.log(`sending: ${val}`);
+
+    sock.write(val);
+    
+        });
+    });
+});
